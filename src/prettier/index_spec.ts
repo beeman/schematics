4b4b58c2708896bb967e83a43325ed5be5885227
Bram Borggreve <borggreve@gmail.com>
2018-05-02 19:39:55 -0500

    chore: configure Semantic Release

--- src/prettier/index_spec.ts
+++ src/prettier/index_spec.ts
@@ -2,15 +2,12 @@ import { Tree } from '@angular-devkit/schematics';
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
 
-
 const collectionPath = path.join(__dirname, '../collection.json');
 
-
 describe('prettier', () => {
-  it('works', () => {
+  it('add .prettierrc', () => {
     const runner = new SchematicTestRunner('prettier', collectionPath);
     const tree = runner.runSchematic('prettier', {}, Tree.empty());
-
-    expect(tree.files).toEqual([]);
+    expect(tree.files).toEqual(['/.prettierrc']);
   });
 });
